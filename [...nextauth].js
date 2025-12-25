2492253d8de5919c94cb6fec21472a1901282467
ahmedmkn999-alt <ahmedmkn999@gmail.com>
2025-12-25 05:00:29 +0200

    Update GoogleProvider client secret and ID comments

--- [...nextauth].js
+++ [...nextauth].js
@@ -4,11 +4,11 @@ import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
   providers: [
     GoogleProvider({
-      // هذا هو الرقم الذي نسخته أنت من الصورة
+      // معرف العميل الذي استخرجته أنت من الصورة
       clientId: "627697628144-jhn1uu9825kqhb1qjqbtiu5joecu07sv.apps.googleusercontent.com",
-      // ضع هنا الرقم السري الثاني (Secret) الذي ظهر لك تحت الرقم السابق في جوجل
+      // ضع هنا الرقم السري الثاني الذي ظهر لك في جوجل
       clientSecret: "ضـع_هنا_Secret_الذي_ظهر_لك_في_جوجل",
     }),
   ],
-  secret: "vidara-secure-key-2025",
+  secret: "vidara-ai-platform-2025",
 });
